0f41b156dc9d508c3118ecd0302433804d231182
Missi2 <a.si2.0121.dpgt@icloud.com>
2022-06-10 12:59:59 +0900

    コメントアウト箇所削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,22 +13,6 @@ class User < ApplicationRecord
     validates :password, format: { with: /\A[a-z0-9]+\z/i, message: 'は半角英数のみ定義できます' }
   end
 
-  # with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
-  #   validates :first_name
-  #   validates :last_name
-  # end
-
-  # with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角カナを使用してください' } do
-  #   validates :first_name_kana
-  #   validates :last_name_kana
-  # end
-
-  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  # validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
-
-  # VALID_PASSWORD_REGEX2 = /\A[a-z0-9]+\z/i
-  # validates :password, format: { with: VALID_PASSWORD_REGEX2, message: 'は半角英数のみ定義できます' }
-
   has_many :items
   has_many :orders
 
